cb2e033d56e6d686a9ccea619c1c6b00fc41d49e
kl2300033362 <2300033362@kluniversity.in>
2026-09-12 23:46:28 +0530

    Update repository name to Edu-platform

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Car-rental/',
+  base: '/Edu-platform/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
